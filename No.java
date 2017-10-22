3d82d00074326bff1f12ef3fbd2ccd587c9e9e4b
Luann F Elias <30964881+luannelias@users.noreply.github.com>
2017-10-22 14:39:00 -0200

    Update No.java

--- No.java
+++ No.java
@@ -1,28 +1,32 @@
-package listas;
+package listase;
 
-public class No {
-	private int conteudo;
-	private No prox;
+public class No 
+{
+    private int conteudo;
+    private No prox;
 	
-	public No(){
-		setProx(null);
-	}
+    public No()
+    {
+        setProx(null);
+    }
 
-	public int getConteudo() {
-		return conteudo;
-	}
+    public int getConteudo() 
+    {
+        return conteudo;
+    }
 
-	public void setConteudo(int conteudo) {
-		this.conteudo = conteudo;
-	}
+    public void setConteudo(int conteudo) 
+    {
+        this.conteudo = conteudo;
+    }
 
-	public No getProx() {
-		return prox;
-	}
+    public No getProx() 
+    {
+        return prox;
+    }
 
-	public void setProx(No prox) {
-		this.prox = prox;
-	}
-	
-	
+    public void setProx(No prox) 
+    {
+        this.prox = prox;
+    }	
 }
